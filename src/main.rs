f03edb0955b68b01168570580392a3b2a93a56a0
longyun <ly1169134156@163.com>
2019-08-16 16:31:10 +0800

    add some property

--- src/main.rs
+++ src/main.rs
@@ -1,13 +1,15 @@
+use exitfailure::ExitFailure;
+use failure::ResultExt;
 use std::fs;
 use std::path::PathBuf;
 use structopt::StructOpt;
-use exitfailure::ExitFailure;
-use failure::ResultExt;
 
 #[derive(StructOpt, Debug)]
+#[structopt(name = "example", about = "An example of StructOpt usage.")]
 pub struct Cli {
+    #[structopt(short = "p", help = "Search pattern")]
     pattern: String,
-    #[structopt(parse(from_os_str))]
+    #[structopt(name = "file_path", parse(from_os_str), help = "File path")]
     pub path: PathBuf,
 }
 
@@ -15,10 +17,6 @@ fn main() -> Result<(), ExitFailure> {
     let args = Cli::from_args();
     let content = fs::read_to_string(&args.path)
         .with_context(|_| format!("could not read file `{}`", args.path.display()))?;
-    grrs::find_matches(
-        &content,
-        &args.pattern,
-        &mut std::io::stdout(),
-    )?;
+    grrs::find_matches(&content, &args.pattern, &mut std::io::stdout())?;
     Ok(())
 }
